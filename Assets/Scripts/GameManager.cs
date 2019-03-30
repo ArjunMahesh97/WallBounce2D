c4a8e5899d39473f9aeae00a6f2c8b319e061815
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-03-30 15:29:12 +0530

    PC build

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    int score = 0;
+    public Text scoreText;
+
+    public GameObject gameOverPanel;
+    public GameObject gameOverEffectPanel;
+
+    private void Awake()
+    {
+        Time.timeScale = 1f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+    public void GameOver()
+    {
+        StartCoroutine(GameOverCoroutine());
+    }
+
+    IEnumerator GameOverCoroutine()
+    {
+        gameOverEffectPanel.SetActive(true);
+        Time.timeScale = 0.1f;
+        yield return new WaitForSecondsRealtime(0.5f);
+        gameOverPanel.SetActive(true);
+        yield break;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     public GameObject WallBounceEffectObj;
     public GameObject DeathEffectObj;
+    public GameObject GameManagerObj;
 
     Rigidbody2D rigidBody;
 
@@ -14,15 +15,26 @@ public class Player : MonoBehaviour
 
     bool isDead = false;
 
+    public CameraShake cameraShake;
+
+    GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        gameManager = GameManagerObj.GetComponent<GameManager>();
+        Debug.Log(gameManager);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isDead)
+        {
+            return;
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
             if (rigidBody.velocity.x > 0)
@@ -40,6 +52,11 @@ public class Player : MonoBehaviour
     {
         if (collision.gameObject.tag == "Walls")
         {
+            if(collision.gameObject.name=="Left" || collision.gameObject.name == "Right")
+            {
+                gameManager.AddScore();
+            }
+
             GameObject effect = Instantiate(WallBounceEffectObj, collision.contacts[0].point, Quaternion.identity);
 
             Destroy(effect, 0.5f);
@@ -51,10 +68,12 @@ public class Player : MonoBehaviour
             GameObject effect = Instantiate(DeathEffectObj, collision.contacts[0].point, Quaternion.identity);
             Destroy(effect, 0.5f);
 
+            gameManager.GameOver();
+            StartCoroutine(cameraShake.Shake(0.2f,0.3f));
+
             rigidBody.velocity = new Vector2(0f, 0f);
             rigidBody.isKinematic = true;
-            gameObject.SetActive(false);
-
+            //gameObject.SetActive(false);
         }
 
         
